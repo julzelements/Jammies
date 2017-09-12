db53711ae452cfb7e45c2003c2ecf4d5645eb092
Julian Scharf <jscharf@thoughtworks.com>
2017-09-12 11:00:21 +1000

    Initial UI is working

--- Jammies/ViewController.swift
+++ Jammies/ViewController.swift
@@ -10,16 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    var timeInterval = 0.01
+    var microphoneRecordingTime = 5.0
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var userFeedbackLabel: UILabel!
+    
+    enum DisplayState {
+        case notRecording
+        case recording
+        case networkRequest
+        case networkFinished
+    }
+    
+    func updateDisplay(displayState: DisplayState) {
+        switch displayState {
+        case .notRecording:
+            userFeedbackLabel.isHidden = true
+        case .recording:
+            userFeedbackLabel.isHidden = false
+            userFeedbackLabel.text = "Recording \(Int(microphoneRecordingTime)) secs of audio..."
+        case .networkRequest:
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+            userFeedbackLabel.text = "Analysing audio..."
+        case .networkFinished:
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            userFeedbackLabel.text = "Analysis complete"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDisplay(displayState: .notRecording)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func didStartListening(_ sender: Any) {
+        progressBar.progress = 0
+        timer = Timer.init(timeInterval: timeInterval, target: self, selector: #selector(updateProgressBar), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer, forMode: .defaultRunLoopMode)
+        updateDisplay(displayState: .recording)
     }
-
+    
+    @objc func updateProgressBar() {
+        if progressBar.progress >= 1.0 {
+            timer.invalidate()
+            updateDisplay(displayState: .networkRequest)
+        } else {
+            progressBar.progress = progressBar.progress + getProgressBarIncrement()
+        }
+    }
+    
+    func getProgressBarIncrement() -> Float {
+        return Float(1 / (microphoneRecordingTime * (1 / timeInterval)))
+    }
+    
 
 }
 
