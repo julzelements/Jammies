fda6b7f42f3b440689e566a48a4ae6f0516a1477
Julian Scharf <jscharf@thoughtworks.com>
2017-09-18 15:12:23 +1000

    added a lot of files from the TimeMock Project

---
+++ AudioRecording/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//RecordedAudio.swift
+
+import Foundation
+
+class RecordedAudio {
+  
+  var filePathURL: NSURL!
+  var title: String!
+  
+  init(audioFilePathURL: NSURL, audioTitle: String) {
+    filePathURL = audioFilePathURL
+    title = audioTitle
+  }
+  
+}
---
+++ JammiesTests/SubtitleIOTests.swift
@@ -0,0 +1,24 @@
+//
+//  SubtitleIOTests.swift
+//  TimeMock
+//
+//  Created by Julian Scharf on 13/7/17.
+//  Copyright © 2017 Julian Scharf. All rights reserved.
+//
+
+import XCTest
+
+class SubtitleIOTests: XCTestCase {
+       
+    func testShouldGetBadURLErrorMessageWhenTryingToGetNonExistentFileFromBundle() {
+        let expectedError = "could not obtain URL"
+        let request = SubtitleIO.getRawStringFromFileInBundle(fileName: "foo", fileExtension: "bar")
+        XCTAssertEqual(expectedError, request)
+    }
+
+    func testShouldGetBadURLErrorMessageWhenTryingToReadAMOVFileAsAString() {
+        let expectedError = "could not read contents of URL as a string"
+        let request = SubtitleIO.getRawStringFromFileInBundle(fileName: "spiderman", fileExtension: "mov")
+        XCTAssertEqual(expectedError, request)
+    }
+}
